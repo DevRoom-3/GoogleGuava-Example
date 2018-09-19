25b7302d11f6ba46df66ed0606f8899d7acdaf3e
jiayangchen <chenjiayang1207@163.com>
2018-09-19 16:48:16 +0800

    add cache

--- src/Cache/GuavaCacheTest.java
+++ src/Cache/GuavaCacheTest.java
@@ -1,7 +1,41 @@
 package Cache;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Logger;
+
 /**
  * create by chenjiayang on 2018/9/16
  */
 public class GuavaCacheTest {
+    private static Logger logger = Logger.getLogger(GuavaCacheTest.class.getName());
+
+    public static void main(String[] args) {
+        /**
+         * A LoadingCache is a Cache built with an attached CacheLoader
+         */
+        LoadingCache<String, String> cache = CacheBuilder.newBuilder()
+                .maximumSize(1000)
+                .expireAfterWrite(10, TimeUnit.MINUTES)
+                .build(new CacheLoader<String, String>() {
+                    @Override
+                    public String load(String s) throws Exception {
+                        return "value";
+                    }
+                });
+        try {
+            //This will either return an already cached value, or else use the
+            // cache's CacheLoader to atomically load a new value into the cache
+            // 如果 Key 不存在的话通过 load 方法返回 value
+            String res = cache.get("key");
+            System.out.println(res);
+        } catch (ExecutionException e) {
+            logger.info(e.getMessage());
+        }
+
+    }
 }
