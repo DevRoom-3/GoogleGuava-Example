f648fa7f5c782b50535eb06d9c06d8c5405a83f8
jiayangchen <chenjiayang1207@163.com>
2018-09-20 14:18:50 +0800

    增加Callable回调实例

--- src/Cache/GuavaCacheTest.java
+++ src/Cache/GuavaCacheTest.java
@@ -1,9 +1,11 @@
 package Cache;
 
+import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
@@ -37,5 +39,28 @@ public class GuavaCacheTest {
             logger.info(e.getMessage());
         }
 
+        /**
+         * get method with Callable
+         */
+        Cache<String, String> cache1 = CacheBuilder.newBuilder()
+                .maximumSize(1000)
+                .initialCapacity(10)
+                .concurrencyLevel(6)
+                .expireAfterAccess(10, TimeUnit.SECONDS)
+                .build();
+        try {
+            String res = cache1.getIfPresent("key");
+            System.out.println(res);
+            // get 方法第二个参数为 Callable 回调函数
+            String res1 = cache1.get("key", new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    return "value";
+                }
+            });
+            System.out.println(res1);
+        } catch (ExecutionException e) {
+            logger.info(e.getMessage());
+        }
     }
 }
